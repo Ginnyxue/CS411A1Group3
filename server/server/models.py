fa8788899c5e6c6acb8bdff64d12ba78496ac4a7
YidingOu <yidingou@126.com>
2017-04-11 17:14:01 -0400

    database setup

---
+++ server/server/models.py
@@ -0,0 +1,9 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class SearchResult(models.Model):
+    search = models.CharField(max_length=200)
+    result = models.CharField(max_length=2000)
